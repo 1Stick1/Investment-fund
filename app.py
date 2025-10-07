b8c40ab43e8577826bf585cc23efc39c0fe7843a
Roman Lysak <lysakroman.m@gmail.com>
2025-10-07 14:51:03 +0200

    Fixed "About us" page. Cleaned and fixed css

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def login():
 def registration():
     return render_template("registration.html")
 
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
