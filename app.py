050f1709b7990075f71f0724e177e68724213bd1
Roman Lysak <lysakroman.m@gmail.com>
2025-10-09 12:22:57 +0200

    Modified unfinished file customer-panel.html

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import yfinance as yf
+import datetime
 
 app = Flask(__name__)
 
@@ -17,6 +19,26 @@ def registration():
 @app.route("/about_us")
 def about_us():
     return render_template("about_us.html")
+
+
+@app.route("/customer_panel")
+def customer_panel():
+    return render_template("customer-panel.html")
+
+
+@app.route("/data")
+def data():
+    # Добавляем новую точку
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+    value = random.randint(1000, 5000)
+    data_points.append({"time": now, "value": value})
+    
+    # Оставляем только последние 10 точек
+    if len(data_points) > 10:
+        data_points.pop(0)
+    
+    return jsonify(data_points)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
