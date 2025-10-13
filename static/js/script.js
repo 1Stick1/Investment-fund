915f3b86034fdf5969221fcc723f1fa077330bd6
sickoshyne044 <brzhilin@gmail.com>
2025-10-13 13:17:03 +0200

    Added password verification to the registration page

--- static/js/script.js
+++ static/js/script.js
@@ -26,3 +26,33 @@ totalSpan.textContent = `${finalAmount.toFixed(2)} ${currency}`;
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button").addEventListener("click", calculate);
 });
+
+
+const form = document.getElementById('signup');
+const password = document.getElementById('password');
+const password_again = document.getElementById('password_again');
+const msg = document.getElementById('msg');
+
+const pwRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}$/;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const pw = password.value;
+  const pw2 = password_again.value;
+
+  if (!pwRegex.test(pw)) {
+    msg.textContent = "❌ Hasło musi mieć co najmniej 8 znaków, zawierać małą i wielką literę oraz symbol (np. !@#).";
+    msg.style.color = "red";
+    return;
+  }
+
+  if (pw !== pw2) {
+    msg.textContent = "❌ Hasła nie pasują.";
+    msg.style.color = "red";
+    return;
+  }
+  msg.textContent = "✅ Hasła pasują!";
+  msg.style.color = "green";
+  form.submit();
+});
